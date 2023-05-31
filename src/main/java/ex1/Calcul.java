4d941e478b82f40edd72d683c909439a06d11841
arny-wilk <arnywilk@gmail.com>
2023-05-31 23:55:05 +0200

    TP Clean code exo1

---
+++ src/main/java/ex1/Calcul.java
@@ -0,0 +1,22 @@
+package ex1;
+
+public class Calcul {
+
+    /**
+     * @param a
+     * @param b
+     * @return addition des deux nombres a et b
+     */
+    public int addition(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * @param a
+     * @param b
+     * @return soustraction des deux nombres a et b
+     */
+    public int soustraction(int a, int b) {
+        return a - b;
+    }
+}
